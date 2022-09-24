8af8472cc716635ebb0e170a56a4bfb4d9de9e8f
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-09-24 16:10:57 +0300

    Done ex04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (...args) {
+  const res = args[0];
 
+  args.map((el) => {
+    if (Array.isArray(el) || !el || res.indexOf(el) === -1) return;
+
+    res.splice(res.indexOf(el), 1);
+  });
+
+  return res;
 };
 
 // Do not edit below this line
