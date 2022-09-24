a3bd2394f05270aada833fe830a18969d58ae0d8
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-09-24 14:01:23 +0300

    Done ex3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (text) {
+  return text.split('').reverse().join('');
 
+  // const res = [];
+
+  // for (i = 0, j = text.length - 1; text.length - 1 >= i; i++, j--) {
+  //   res[i] = text[j];
+  // }
+
+  // return res.join('');
 };
 
 // Do not edit below this line
