96a1632c1b4c22c712b0b5c5981f78461c957e1b
Nikita Krivoshei <NikitaKrivoshei@gmail.com>
2022-09-25 14:22:52 +0300

    Done ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (fromNum, toNum) {
+  if (
+    fromNum < 0 ||
+    toNum < 0 ||
+    typeof fromNum !== 'number' ||
+    typeof toNum !== 'number'
+  )
+    return 'ERROR';
 
+  if (fromNum > toNum) {
+    let i = fromNum;
+    fromNum = toNum;
+    toNum = i;
+  }
+
+  let sum = 0;
+
+  for (let i = fromNum; i <= toNum; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
